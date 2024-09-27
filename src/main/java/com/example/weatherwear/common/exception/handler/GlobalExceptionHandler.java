8b06d35edb1e6cb7c60093c5bccfb53017e21338
mini <155148431+mini0192@users.noreply.github.com>
2024-09-28 01:09:16 +0900

    Feat: create Exception Handler and AuthenticationException Handler

--- src/main/java/com/example/weatherwear/common/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/weatherwear/common/exception/handler/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -19,6 +20,14 @@ import java.util.List;
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+//    Todo: 개발하면서 테스트 진행
+//    모든 예외 처리
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<HttpErrorInfo> handleGenericException(Exception ex, HttpServletRequest request) {
+        String message = "알 수 없는 오류가 발생했습니다.";
+        return createExceptionResponse(HttpStatus.INTERNAL_SERVER_ERROR, request, message);
+    }
 
 //    404
     @ExceptionHandler(NoHandlerFoundException.class)
@@ -34,17 +43,27 @@ public class GlobalExceptionHandler {
         return createExceptionResponse(HttpStatus.METHOD_NOT_ALLOWED, request, message);
     }
 
+//    Todo: Security 설정이 마무리 되면 테스트
+//    권한이 없음
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<HttpErrorInfo> handleAuthenticationException(AuthenticationException ex, HttpServletRequest request) {
+        String message = "해당 요청의 권한이 없습니다.";
+        return createExceptionResponse(HttpStatus.METHOD_NOT_ALLOWED, request, message);
+    }
+
+//    Todo: 개발하면서 테스트 진행
 //    유효성 검사 실패
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<HttpErrorInfo> handlerMethodArgumentNotValidException(MethodArgumentNotValidException ex, HttpServletRequest request) {
         List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
         String message = fieldErrors.stream()
-                .map(error -> error.getField() + ", " + error.getDefaultMessage())
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
                 .toString();
 
         return createExceptionResponse(HttpStatus.BAD_REQUEST, request, message);
     }
 
+//    Todo: 개발하면서 테스트 진행
 //    해당 데이터를 조회 할 수 없음
     @ExceptionHandler(NotFoundDataException.class)
     public ResponseEntity<HttpErrorInfo> handlerNotFoundDataException(NotFoundDataException ex, HttpServletRequest request) {
@@ -52,6 +71,7 @@ public class GlobalExceptionHandler {
         return createExceptionResponse(HttpStatus.BAD_REQUEST, request, message);
     }
 
+//    Todo: 개발하면서 테스트 진행
 //    DB 접근 예외
     @ExceptionHandler(DataAccessException.class)
     public ResponseEntity<HttpErrorInfo>  handleDataAccessException(DataAccessException ex, HttpServletRequest request) {
@@ -59,6 +79,7 @@ public class GlobalExceptionHandler {
         return createExceptionResponse(HttpStatus.BAD_REQUEST, request, message);
     }
 
+//    Todo: 개발하면서 테스트 진행
 //    DB 무결성 재약 조건 위배
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<HttpErrorInfo> handlerDataIntegrityViolationException(DataIntegrityViolationException ex, HttpServletRequest request) {
